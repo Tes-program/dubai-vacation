9d8e7a3e8dee2c13c9e166bd95d14771d96e041f
Tes-program <teslimodumuyiwa@gmail.com>
2023-06-09 23:19:06 +0100

    Created Home and Package Section

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', function() {
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('scrolling-active', windowPosition);
+});
+
